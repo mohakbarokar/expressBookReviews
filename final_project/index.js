a6f7673eb1935b364a08b9ef5d3103391f0b1e06
Mohak Barokar <mohak.barokar@ril.com>
2023-07-09 11:20:32 +0530

    Task 1 completed

--- final_project/index.js
+++ final_project/index.js
@@ -4,19 +4,95 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+let users = []
+
+//Function to check if the user exists
+const doesExist = (username) => {
+    let userswithsamename = users.filter((user) => {
+        return user.username === username
+    });
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Function to check if the user is authenticated
+const authenticatedUser = (username, password) => {
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password)
+    });
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) { //get the authorization object stored in the session
+        token = req.session.authorization['accessToken']; //retrieve the token from authorization object
+        jwt.verify(token, "access", (err, user) => { //Use JWT to verify token
+            if (!err) {
+                req.user = user;
+                next();
+            }
+            else {
+                return res.status(403).json({ message: "User not authenticated" })
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" })
+    }
 });
- 
-const PORT =5000;
+
+app.post("/login", (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+});
+
+app.post("/register", (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+
+    if (username && password) {
+        if (!doesExist(username)) {
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registred. Now you can login" });
+        } else {
+            return res.status(404).json({ message: "User already exists!" });
+        }
+    }
+    return res.status(404).json({ message: "Unable to register user." });
+});
+
+const PORT = 5050;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
