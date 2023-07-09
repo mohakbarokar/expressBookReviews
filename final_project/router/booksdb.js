f5d21a1d1c503cfd899a68eaa3e62e30ff758a12
Mohak Barokar <mohak.barokar@ril.com>
2023-07-09 11:47:07 +0530

    Added task 2 code

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,66 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+let books = [
+      {
+            "isbn": "9781593279509",
+            "title": "Eloquent JavaScript, Third Edition",
+            "subtitle": "A Modern Introduction to Programming",
+            "author": "Marijn Haverbeke",
+            "published": "2018-12-04T00:00:00.000Z",
+            "publisher": "No Starch Press",
+            "pages": 472,
+            "reviews": [
+                  { "reviewUser": "default", "review": "JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications." }],
+            "website": "http://eloquentjavascript.net/"
+      },
+      {
+            "isbn": "9781491943533",
+            "title": "Practical Modern JavaScript",
+            "subtitle": "Dive into ES6 and the Future of JavaScript",
+            "author": "Nicolás Bevacqua",
+            "published": "2017-07-16T00:00:00.000Z",
+            "publisher": "O'Reilly Media",
+            "pages": 334,
+            "reviews": [
+                  { "reviewUser": "default", "review": "To get the most out of modern JavaScript, you need learn the latest features of its parent specification, ECMAScript 6 (ES6). This book provides a highly practical look at ES6, without getting lost in the specification or its implementation details." }],
+            "website": "https://github.com/mjavascript/practical-modern-javascript",
+      },
+      {
+            "isbn": "9781593277574",
+            "title": "Understanding ECMAScript 6",
+            "subtitle": "The Definitive Guide for JavaScript Developers",
+            "author": "Nicholas C. Zakas",
+            "published": "2016-09-03T00:00:00.000Z",
+            "publisher": "No Starch Press",
+            "pages": 352,
+            "reviews": [
+                  { "reviewUser": "default", "review": "ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that ECMAScript 6 brings to JavaScript." }],
+            "website": "https://leanpub.com/understandinges6/read",
+      },
+      {
+            "isbn": "9781449365035",
+            "title": "Speaking JavaScript",
+            "subtitle": "An In-Depth Guide for Programmers",
+            "author": "Axel Rauschmayer",
+            "published": "2014-04-08T00:00:00.000Z",
+            "publisher": "O'Reilly Media",
+            "pages": 460,
+            "reviews": [
+                  { "reviewUser": "default", "review": "Like it or not, JavaScript is everywhere these days -from browser to server to mobile- and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who once found himself in the same position." }],
+            "website": "http://speakingjs.com/",
+            "reviewUser": "default"
+      },
+      {
+            "isbn": "9781449331818",
+            "title": "Learning JavaScript Design Patterns",
+            "subtitle": "A JavaScript and jQuery Developer's Guide",
+            "author": "Addy Osmani",
+            "published": "2012-08-30T00:00:00.000Z",
+            "publisher": "O'Reilly Media",
+            "pages": 254,
+            "review": "With Learning JavaScript Design Patterns, you'll learn how to write beautiful, structured, and maintainable JavaScript by applying classical and modern design patterns to the language. If you want to keep your code efficient, more manageable, and up-to-date with the latest best practices, this book is for you.",
+            "website": "http://www.addyosmani.com/resources/essentialjsdesignpatterns/book/",
+            "reviewUser": "default"
+      }
+];
 
-module.exports=books;
+
+module.exports = books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,57 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  //Write your code here
   return res.status(300).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  let filtered_books = books.filter((book) => book.isbn === isbn);
+  if (filtered_books.length > 0) {
+    return res.status(300).json(filtered_books);
+  } else {
+    return res.status(404).json({ "status": 404, "message": "Book not found" });
+  }
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const authorName = req.params.author;
+  let filtered_books = books.filter((book) => book.author === authorName);
+  if (filtered_books.length > 0) {
+    return res.status(300).json(filtered_books);
+  } else {
+    return res.status(404).json({ "status": 404, "message": "Book not found" });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  let filtered_books = books.filter((book) => book.title === title);
+  if (filtered_books.length > 0) {
+    return res.status(300).json(filtered_books);
+  } else {
+    return res.status(404).json({ "status": 404, "message": "Book not found" });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  let filtered_books = books.filter((book) => book.isbn === isbn);
+  if (filtered_books.length > 0) {
+    let filtered_reviews = [];
+    filtered_books.forEach(function (book) {
+      console.log(book.review);
+      filtered_reviews.push({ "isbn": book.isbn, "title": book.title, "reviews": book.reviews });
+    });
+    return res.status(300).json(filtered_reviews);
+  } else {
+    return res.status(404).json({ "status": 404, "message": "Book not found for isbn " + isbn });
+  }
 });
 
 module.exports.general = public_users;
